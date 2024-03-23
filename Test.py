317692089cae01a8274d35a39b12f7d2e19a2c4d
mohamed HJ <83469444+mohamed-HJ@users.noreply.github.com>
2024-03-23 00:03:20 +0000

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,75 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import scipy
+# Define directories for training and validation data
+train_dir = 'C:/Users/Med/Downloads/cats_and_dogs_filtered/cats_and_dogs_filtered/train'
+validation_dir = 'C:/Users/Med/Downloads/cats_and_dogs_filtered/cats_and_dogs_filtered/validation'
+
+# Define image dimensions and batch size
+img_width, img_height = 150, 150
+batch_size = 32
+
+# Create data generators with data augmentation for training and validation
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary'
+)
+
+validation_generator = validation_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary'
+)
+
+# Define and compile the CNN model
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(img_width, img_height, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model and store the training history
+history = model.fit(
+    train_generator,
+    steps_per_epoch=train_generator.samples // batch_size,
+    epochs=50,
+    validation_data=validation_generator,
+    validation_steps=validation_generator.samples // batch_size
+)
+
+# Save the model
+model.save('C:\\Users\\Med\\Desktop\\STRI\\Tensorflow\\model')
+
+
+# Save the training history
+import pickle
+with open('training_history.pkl', 'wb') as f:
+    pickle.dump(history.history, f)
